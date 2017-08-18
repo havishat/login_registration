1ea09b4573cdbec88120c3fac9a2790ba04128f5
havishat <sundariha@gmail.com>
2017-08-17 21:46:49 -0700

    add

---
+++ apps/lr/views.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# Create your views here.
+from django.shortcuts import render, HttpResponse, redirect
+from models import User
+from django.contrib import messages
+# the index function is called when root is visited
+def index(request):
+    return render(request, "lr/index.html")
+
+def create(request, ):
+    result = User.objects.validate_registration(request.POST)
+    if type(result) == list:
+        for err in result:
+            messages.error(request, err)
+        return redirect('/')
+    request.session['user_id'] = result.id
+    messages.success(request, "Successfully registered!")
+    return redirect('/success')
+
+def login(request):
+    result = User.objects.validate_login(request.POST)
+    if type(result) == list:
+        for err in result:
+            messages.error(request, err)
+        return redirect('/')
+    request.session['user_id'] = result.id
+    messages.success(request, "Successfully logged in!")
+    return redirect('/success')
+
+def success(request):
+    try:
+        request.session['user_id']
+    except KeyError:
+        return redirect('/')
+    context = {
+        'user': User.objects.get(id=request.session['user_id'])
+    }
+    return render(request, 'lr/success.html', context)
